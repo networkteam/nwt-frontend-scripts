3dd67b41db60f8c0a28e6849c94e1ef837c381cd
Rasmus Schmidt <rasmus.schmidt@networkteam.com>
2022-10-18 16:54:24 +0200

    fixes: deep merges plugins instead of shallow

--- helpers/configHelpers.js
+++ helpers/configHelpers.js
@@ -33,7 +33,7 @@ function deleteRemovedKeys(defaultConfig, customConfig) {
 }
 
 /**
- * merges plugins of customConfig in defaultConfig and deletes customConf.plugins afterwards, so that they are being initialized only once.
+ * merges plugins of customConfig in defaultConfig if they were also defined in defaultConfig then removes them from initialization array once, so that they are being initialized only once.
 
  * @param {object} defaultConfig default configuration in current environment
  * @param {object} customConfig custom configuration from project in current environment
@@ -44,20 +44,52 @@ function mergeConfigPlugins(defaultConfig, customConfig) {
   let customConf = customConfig;
 
   if (customConf.plugins) {
-    defaultConf.plugins =
-      defaultConf.plugins.map((plugin) => {
-        const customPlugin = customConf.plugins.find(
-          (customPlugin) => customPlugin.constructor.name === plugin.constructor.name
-        );
-        if (customPlugin) {
-          return Object.assign(plugin, customPlugin);
-        }
-        return plugin;
-      })
-
-      delete customConf.plugins;
+    defaultConf.plugins = defaultConf.plugins.map((plugin) => {
+      // finds duplicated plugin in custom config
+      const customPlugin = customConf.plugins.find(
+        (customPlugin) => customPlugin.constructor.name === plugin.constructor.name
+      );
+      if (customPlugin) {
+        const mergedPlugin = mergeDeep(plugin, customPlugin);
+        // removes plugin from custom config so it is not initialized twice
+        customConf.plugins.splice(customConf.plugins.indexOf(customPlugin), 1);
+        return mergedPlugin
+      }
+      return plugin;
+    });
   }
   return {defaultConf, customConf};
+}
+
+function isObject(item) {
+  return item && typeof item === 'object' && !Array.isArray(item);
+}
+
+/**
+ * recursively merges two objects by keys
+
+ * @param {object} target target object to merge into
+ * @param {object} sources target object to merge from
+ */
+
+function mergeDeep(target, ...sources) {
+  if (!sources.length) return target;
+  const source = sources.shift();
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) Object.assign(target, {
+          [key]: {}
+        });
+        mergeDeep(target[key], source[key]);
+      } else {
+        Object.assign(target, {
+          [key]: source[key]
+        });
+      }
+    }
+  }
+  return mergeDeep(target, ...sources);
 }
 
 function getOutputPath(environment) {
